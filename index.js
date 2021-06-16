0d45e05beb4185e2432a71c94664951affa1d081
David Deaton <dideater@gmail.com>
2021-06-15 19:00:48 -0700

    made Seminars and Webinars more mobile friendly

--- index.js
+++ index.js
@@ -33,3 +33,63 @@ $("#noClick").click(() => {
     }, 12000);
 
 });
+
+$("#osMore").click(() => {
+
+    $("#osSummary").removeClass("summary");
+    $("#osSummary").addClass("full");
+
+    $("#os").removeClass("full");
+    $("#os").addClass("summary");
+
+});
+
+$("#osHide").click(() => {
+
+    $("#os").addClass("full");
+    $("#os").removeClass("summary");
+
+    $("#osSummary").addClass("summary");
+    $("#osSummary").removeClass("full");
+
+});
+
+$("#dtMore").click(() => {
+
+    $("#dtSummary").removeClass("summary");
+    $("#dtSummary").addClass("full");
+
+    $("#dt").removeClass("full");
+    $("#dt").addClass("summary");
+
+});
+
+$("#dtHide").click(() => {
+
+    $("#dt").addClass("full");
+    $("#dt").removeClass("summary");
+
+    $("#dtSummary").addClass("summary");
+    $("#dtSummary").removeClass("full");
+
+});
+
+$("#vMore").click(() => {
+
+    $("#vSummary").removeClass("summary");
+    $("#vSummary").addClass("full");
+
+    $("#v").removeClass("full");
+    $("#v").addClass("summary");
+
+});
+
+$("#vHide").click(() => {
+
+    $("#v").addClass("full");
+    $("#v").removeClass("summary");
+
+    $("#vSummary").addClass("summary");
+    $("#vSummary").removeClass("full");
+
+});
